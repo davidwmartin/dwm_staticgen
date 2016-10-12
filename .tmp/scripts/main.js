84cfbf86a73877e6dda922799f0fdfe23adf7a13
David Martin <david.w.martin@me.com>
2016-10-12 11:46:43 -0700

    working through TODOs -- now using .tmp structure for compiled-but-not-processed-for-dev assets

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,1 @@
+console.log('hello world')null//# sourceMappingURL=main.js.mapnull
--- gulpfile.js
+++ gulpfile.js
@@ -8,24 +8,14 @@ var gulp = require('gulp'),
 		imagemin = require('gulp-imagemin'), 
 		cache = require('gulp-cache'), 
 		del = require('del'), 
-		runSequence = require('run-sequence')
+		runSequence = require('run-sequence'), 
+		autoprefixer = require('gulp-autoprefixer'), 
+		sourcemaps = require('gulp-sourcemaps'), 
+		eslint = require('gulp-eslint');
 		;
 
-// compile scss files and pipe to css folder
-gulp.task('sass', function(){
-
-	return gulp.src('app/assets/scss/**/*.+(scss|sass)')
-		.pipe(sass())
-		.pipe(gulp.dest('app/assets/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
-
-});
-
-
 // configure watch task
-gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.task('watch', ['browserSync', 'sass', 'scripts'], function(){
 
 	gulp.watch('app/assets/scss/**/*.scss', ['sass']);
 
@@ -35,23 +25,61 @@ gulp.task('watch', ['browserSync', 'sass'], function(){
 
 });
 
+// compile sass files
+gulp.task('sass', function(){
 
-// setup browser-sync server
-gulp.task('browserSync', function(){
+	return gulp.src('app/assets/scss/**/*.+(scss|sass)')
 
-	browserSync.init({
-		server: {
-			baseDir: 'app'
-		},
-	})
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		// TODO -- sourcemap data to it's own file?
+		.pipe(sourcemaps.write())
+		// TODO -- autoprefixer in usemin task to autoprefix external stylesheets?
+		.pipe(autoprefixer({
+			browsers:['last 2 versions']
+		}))
+		.pipe(gulp.dest('.tmp/styles'))
+		.pipe(browserSync.reload({
+			stream: 	true
+		}));
 
+});
+
+// grab js files, sourcemap them, output to tmp directory
+gulp.task('scripts', function(){
+
+	return gulp.src('app/assets/scripts/**/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('.tmp/scripts'))
+		.pipe(browserSync.reload({
+			stream: 	true
+		}));
+
+});
+
+// Lint JS files with ESLINT
+gulp.task('lint', function(){
+
+	return gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
+		// .pipe(browserSync.reload({
+		// 		stream: true, 
+		// 		once: true
+		// 	}))
+		.pipe(eslint({
+			configFile: 'config/.eslintrc'
+		}))
+		.pipe(eslint.format())
+		// .pipe(gulp.dest('app/scripts'))
 });
 
 // useref combines assets into a single file after piping through minifiers based on file extension
 gulp.task('useref', function(){
 
 	return gulp.src('app/*.html')
-		.pipe(useref())
+		.pipe(useref({
+				searchPath:['.tmp', 'app']
+			}))
 		.pipe(gulpIf('*.js', uglify()))
 		.pipe(gulpIf('*.css', cssnano()))
 		.pipe(gulp.dest('dist'));
@@ -71,9 +99,31 @@ gulp.task('images', function(){
 	
 	return gulp.src('app/assets/images/**/*.+(png|jpg|jpeg|giv|svg)')
 		.pipe(cache(imagemin({
-				interlaced: true
+				// gif options
+				interlaced: true,
+				optimizationLevel: 2,
+				// jpg options
+				progressive: true
 			})))
 		.pipe(gulp.dest('dist/images'));
+
+});
+
+// extras (favicon, robots.txt, other root-level html files, etc)
+gulp.task('extras', function() {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+
+// generic clean task
+gulp.task('clean', function() {
+	  
+	  runSequence('clean:dist', 'clean:cache')
 
 });
 
@@ -85,17 +135,29 @@ gulp.task('clean:dist', function(){
 });
 
 // delete locally cached images
-gulp.task('cache:clear', function (callback) {
+gulp.task('clean:cache', function () {
 
-	return cache.clearAll(callback)
+	return cache.clearAll()
 
 });
 
+// setup browser-sync server
+gulp.task('browserSync', function(){
+
+	browserSync.init({
+		server: {
+			port: 9000,
+			baseDir: ['.tmp','app']
+		},
+	})
+
+});
 
 // serve dev site
-gulp.task('serve', function(callback){
+// TODO -- do I need this? What's the diff btw this and my 'watch' task?
+gulp.task('serve', function(){
 
-	runSequence(['sass', 'browserSync', 'watch'], callback);
+	runSequence(['sass', 'scripts', 'browserSync', 'watch']);
 
 });
 
@@ -103,10 +165,10 @@ gulp.task('serve', function(callback){
 // build production site
 gulp.task('build', function(){
 
-	console.log('building files');
+	console.log('building dist');
 
 	// Note -- runsequence runs tasks separated by comma one-by-one, tasks in [] will be run at the same time
-	runSequence('clean:dist', 'sass', ['useref', 'images'], callback);
+	runSequence('clean', 'sass', 'lint',['useref', 'images', 'extras']);
 
 });
 
